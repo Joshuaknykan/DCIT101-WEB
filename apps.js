27af103699acffb2d06148285c69b4f302bd6ed5
JoshKan <n0thomglikehome@gmail.com>
2022-05-03 20:39:15 +0000

    intial commit of website

---
+++ apps.js
@@ -0,0 +1,84 @@
+const music = new Audio('XXX.mp3');
+
+// create Array 
+
+const songs = [
+    {
+        id:'1',
+        songName:` Sad <br>
+        <div class="subtitle">Xxxtentacion</div>`,
+        poster: "img/1.jpg"
+    },
+    {
+        id:'2',
+        songName:` Moonlight <br>
+        <div class="subtitle">Xxxtentacion</div>`,
+        poster: "img/2.jpg"
+    },
+    // all object type 
+    {
+        id:"3",
+        songName: `Bellyache <br><div class="subtitle"> Billie Eilish</div>`,
+        poster: "img/3.jpg",
+    },
+    {
+        id:"4",
+        songName: `Aim For Moon <br><div class="subtitle">Pop Smoke</div>`,
+        poster: "img/4.jpg",
+    },
+    {
+        id:"5",
+        songName: `Wishing Well <br><div class="subtitle">Juice Wrld</div>`,
+        poster: "img/5.jpg",
+    },
+    {
+        id:"6",
+        songName: `Good News <br><div class="subtitle">Mac Miller</div>`,
+        poster: "img/6.jpg",
+    },
+    {
+        id:"7",
+        songName: `Morbid Mind <br><div class="subtitle">Jack Kays</div>`,
+        poster: "img/7.jpg",
+    },
+    {
+        id:"8",
+        songName: `Dissolve <br><div class="subtitle">Absofacto</div>`,
+        poster: "img/8.jpg",
+    },
+    {
+        id:"9",
+        songName: `Dilber <br><div class="subtitle">Glass Animals</div>`,
+        poster: "img/9.jpg",
+    },
+    {
+        id:"10",
+        songName: `IDGAF <br><div class="subtitle">Boywithuke</div>`,
+        poster: "img/10.jpg",
+    },
+    {
+        id:"11",
+        songName: `Obsessed with You <br><div class="subtitle">Central cee</div>`,
+        poster: "img/11.jpg",
+    },
+    {
+        id:"12",
+        songName: `Snowflake <br><div class="subtitle">Powfu</div>`,
+        poster: "img/12.jpg",
+    },
+    {
+        id:"13",
+        songName: `Baarishein <br><div class="subtitle">Don Toliver</div>`,
+        poster: "img/13.jpg",
+    },
+    {
+        id:"14",
+        songName: `Highest In The School <br><div class="subtitle">Travis Scott</div>`,
+        poster: "img/14.jpg",
+    },
+    {
+        id:"15",
+        songName: `First class <br><div class="subtitle">Jack Harlow</div>`,
+        poster: "img/15.jpg",
+    },
+]
